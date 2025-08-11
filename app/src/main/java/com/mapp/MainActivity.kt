0fbb11f13c8e8a45de74a9774ca23a6e80800264
Larry Moore <hvw@yandex.com>
2025-08-11 18:53:35 +0300

    broken interactions

--- app/src/main/java/com/mapp/MainActivity.kt
+++ app/src/main/java/com/mapp/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -25,33 +28,34 @@ class MainActivity : ComponentActivity() {
         val colors: List<Color> = listOf(Color(0xFF9C27B0),Color(0xFF2196F3),
             Color(0xFF4CAF50),Color(0xFF00BCD4),Color(0xFFFFC107)
         )
-		val randomName = names.random()
-
+		val randomName = mutableStateOf(names.random())
+        fun onClick() = {
+            randomName.value = names.random()
+        }
 		enableEdgeToEdge()
 		setContent {
 			MappTheme {
-				Greeting(randomName,names.random(),names.random(), bg = colors.random())
+				Greeting(randomName,names.random(),names.random(), bg = colors.random(),onClick= onClick())
 			}
 		}
 	}
 }
 
 @Composable
-fun Greeting(name1: String = "Android",name2: String = "Android",name3: String = "Android",bg:Color = Color(0x0ff0ffff)) {
-	Scaffold(modifier = Modifier.fillMaxHeight(), contentColor = Color(0xff000000), containerColor = bg) { innerPadding ->
+fun Greeting(name1: MutableState<String> = mutableStateOf("Android"), name2: String = "Android", name3: String = "Android", bg:Color = Color(0x0ff0ffff), onClick:()->Unit={ println("Hello") }) {
+	Scaffold(modifier = Modifier.fillMaxHeight().clickable(onClick=onClick), contentColor = Color(0xff000000), containerColor = bg) { innerPadding ->
 
 		Column(modifier = Modifier.fillMaxHeight()
-			.padding(horizontal = 32.dp)
-			, verticalArrangement = Arrangement.Center
-
+			.padding(horizontal = 32.dp),
+            verticalArrangement = Arrangement.Center,
 		) {
 			Row(
 				modifier = Modifier
 					.fillMaxWidth()
 					.padding(innerPadding)
 				, horizontalArrangement = Arrangement.SpaceBetween){
-				Text(name1, style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 32.sp))
-				Text(text = name2, style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 32.sp))
+				Text(name1.value, style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 32.sp))
+                Text(text = name2, style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 32.sp))
 				Text(name3, style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 32.sp))
 			}
 		}
@@ -69,6 +73,6 @@ fun GreetingPreview() {
             Color(0xFF4CAF50),Color(0xFF00BCD4),Color(0xFFFFC107)
         )
 		val randomName = names.random()
-		Greeting(randomName,names.random(),names.random(), bg = colors.random())
+		Greeting(mutableStateOf(randomName),names.random(),names.random(), bg = colors.random())
 	}
 }
